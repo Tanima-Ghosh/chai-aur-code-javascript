d5dce57c55ede8f8a6b1be8d890cebc44cf0f40f
Tanima-Ghosh <166041465+Tanima-Ghosh@users.noreply.github.com>
2024-04-19 14:25:21 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treats all code as newer version
+
+// alert(3+3) //alert not work in node js
+
+let name = "Tanima"
+let age = 18
+let isLoggedIn = false
+
+/** premitive data types */
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value/represent empty value
+// undefined => value is not assigned yet
+// symbol => unique
+
+/** reference data types */
+
+// object
+
+console.log(typeof undefined); //o/p: undefined
+console.log(typeof null); // o/p: object
