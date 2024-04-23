352dc220fdba2f203e73591f63866920ab8c8ece
Tanima-Ghosh <166041465+Tanima-Ghosh@users.noreply.github.com>
2024-04-23 06:34:10 +0000

    coversions and operations

---
+++ 01_basics/03_conversionsOperations.js
@@ -0,0 +1,24 @@
+let score = true;
+
+// console.log(score, typeof score);
+
+let valueInNumber = Number(score);
+
+// console.log(valueInNumber, typeof valueInNumber);
+
+/**cnversion into number */
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1
+//false => 0
+
+let isLoggedIn = 1
+
+let booleanInLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanInLoggedIn)
+
+// 1 => true, 0 => false
+// "" => false
+// "john" => true
