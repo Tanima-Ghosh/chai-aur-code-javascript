9ed34d26d33058f7d3428d5baa6ae3ca4c0946c2
Tanima-Ghosh <166041465+Tanima-Ghosh@users.noreply.github.com>
2024-04-26 12:33:41 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+let a = "2"
+let b = 2
+
+let c = parseInt(a)
+let d = Number(a)
+
+console.log(a == b); //js covert the string into number o/p: true
+console.log(Number(a) === b); //false
+
+console.log(typeof c, typeof d);
