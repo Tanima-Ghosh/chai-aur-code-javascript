cba6ecbeddc6be98af0f15a1afe74584c80f70e3
Tanima-Ghosh <166041465+Tanima-Ghosh@users.noreply.github.com>
2024-04-29 05:31:11 +0000

    data types

---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,19 @@
+// primitive  data types
+
+// 7 types: string, number, boolean, null, undefined, symbol, bigInt
+
+// referene type (non premitive)
+
+// arrey, objects, functions
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outSideTemp = null
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id === anotherId);
